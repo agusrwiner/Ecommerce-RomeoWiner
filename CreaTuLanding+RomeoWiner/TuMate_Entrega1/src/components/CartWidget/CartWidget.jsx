cb8d42625d54ce1b04e963ac90bff25fb282ac13
Agustin R. Winer <r.w.agustin@gmail.com>
2023-12-19 20:15:58 -0300

    Subida de archivos base

---
+++ CreaTuLanding+RomeoWiner/TuMate_Entrega1/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import cart from './Assets/react.svg';
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={cart} alt="" />
+            0
+        </div>
+    )
+}
+
+export default CartWidget
