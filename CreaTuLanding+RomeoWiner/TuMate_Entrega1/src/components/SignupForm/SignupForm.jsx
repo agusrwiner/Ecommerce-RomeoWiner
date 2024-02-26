4339c83829c0218356499bb3512bf06631892888
Agustin R. Winer <r.w.agustin@gmail.com>
2024-02-25 23:22:43 -0300

    Creacion SignupForm

--- CreaTuLanding+RomeoWiner/TuMate_Entrega1/src/components/SignupForm/SignupForm.jsx
+++ CreaTuLanding+RomeoWiner/TuMate_Entrega1/src/components/SignupForm/SignupForm.jsx
@@ -1,9 +1,9 @@
-import css from './LoginForm.module.css'
+import css from './SignupForm.module.css'
 
 const LoginForm = () => {
     return(
         <div className={`${css.checlOutFormContainer}`}>
-            <h2 className={`${css.formTitle}`}>Login</h2>
+            <h2 className={`${css.formTitle}`}>Sign up</h2>
             <form className={`${css.form}`}>
                 <div className={`${css.userBox}`}>
                     <input className='userInput' type="text" name="" required />
@@ -17,10 +17,10 @@ const LoginForm = () => {
                     <input className='userInput' type="text" name="" required />
                     <label className='userLabel'>Email</label>
                 </div>
-                {/* <div className={`${css.userBox}`}>
-                            <input className='userInput' type="password" name="" required/>
-                            <label className='userLabel'>Contraseña</label>
-                </div> */}
+                <div className={`${css.userBox}`}>
+                    <input className='userInput' type="password" name="" required/>
+                    <label className='userLabel'>Contraseña</label>
+                </div>
             </form>
         </div>
     )
