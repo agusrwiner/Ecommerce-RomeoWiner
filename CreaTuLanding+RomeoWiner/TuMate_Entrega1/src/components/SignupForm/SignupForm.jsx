932173559d3c5309965e7b7849df2e620c9691f7
Agustin R. Winer <r.w.agustin@gmail.com>
2024-02-28 16:52:19 -0300

    Creacion de Checkout - listo para entregar

--- CreaTuLanding+RomeoWiner/TuMate_Entrega1/src/components/SignupForm/SignupForm.jsx
+++ CreaTuLanding+RomeoWiner/TuMate_Entrega1/src/components/SignupForm/SignupForm.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import css from './SignupForm.module.css'
-import { useAuthContext } from '../../routing/context/authContext';
-
-
 import { db } from '../../firebase/config';
 import { collection, addDoc } from 'firebase/firestore'; 
 import { useCartContext } from '../../routing/context/cartContext';
@@ -10,39 +7,50 @@ import { useNavigate } from 'react-router-dom';
 
 const SignupForm = () => {
     const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-    const { doCreateUserWithEmailAndPassword } = useAuthContext()
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
 
     const ordersCollection = collection(db, "orders");
-    const { cart, clearCart } = useCartContext()
+    const { cart, clearCart, total, itemsTotal } = useCartContext()
     const navigate = useNavigate();
     const order = {
-        'cart': cart,
+        'items': cart,
+        'total': total,
+        'itemsTotal': itemsTotal,
+        'name': name,
+        'surname': surname,
         'email': email
     }
 
-    const handleSubmit = async (e)=> {
+    const createOrder = async (e)=> {
         e.preventDefault()
 
         addDoc( ordersCollection,order ).then((docRef) => {
-        alert(`Oreden realizada con exito! Numero de orden:: ${docRef.id}`)
-        clearCart()
-        navigate('/')
+            const orderId = docRef.id
+            clearCart()
+            navigate(`/checkout/${orderId}`)
         }).catch((error) => {
-            alert(`Ocurrio un error: ${error}`);
+            console.log(`Ocurrio un error: ${error}`);
         });
     }
 
     return(
         <div className={`${css.checlOutFormContainer}`}>
             <h2 className={`${css.formTitle}`}>Complete su email</h2>
-            <form  className={`${css.form}`}>
+            <form className={`${css.form}`}>
                 <div className={`${css.userBox}`}>
-                    <input className='userInput' onChange={ (e)=>setEmail(e.target.value) } type="text" name="" required />
+                    <input className='userInput' onChange={(e) => setSurname(e.target.value)} type="text" name="" required />
+                    <label className='userLabel'>Apellido</label>
+                </div>
+                <div className={`${css.userBox}`}>
+                    <input className='userInput' onChange={(e) => setName(e.target.value)} type="text" name="" required />
+                    <label className='userLabel'>Nombre</label>
+                </div>
+                <div className={`${css.userBox}`}>
+                    <input className='userInput' onChange={(e) => setEmail(e.target.value)} type="text" name="" required />
                     <label className='userLabel'>Email</label>
                 </div>
-                <button onClick={handleSubmit} className={`${css.button}`}>Comprar</button>
+                <button onClick={createOrder} className={`${css.button}`} type='submit' >Comprar</button>
             </form>
         </div>
     )
